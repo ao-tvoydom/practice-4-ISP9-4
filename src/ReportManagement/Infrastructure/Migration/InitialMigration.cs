6ddaf17a488832a8d6bf2d73dfc0d761bc154782
Vadim Larichkin <lvn27112002@mail.ru>
2021-12-03 13:36:04 +0300

    Added InitialMigration

---
+++ src/ReportManagement/Infrastructure/Migration/InitialMigration.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+
+namespace Infrastructure.Migration
+{
+    public class InitialMigration : FluentMigrator.Migration
+    {
+        public override void Up()
+        {
+            this.Execute.EmbeddedScript("UpScript.sql");
+        }
+
+        public override void Down()
+        {
+            this.Execute.EmbeddedScript("DownScript.sql");
+        }
+    }
+}
