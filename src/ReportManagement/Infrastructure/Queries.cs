c4394d2053c11870c7177932b53f782b69e0dc3c
Timeset <drakozavr.hy@mail.ru>
2021-12-06 10:34:42 +0300

    Added Queries Class

---
+++ src/ReportManagement/Infrastructure/Queries.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Dapper;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Infrastructure
+{
+    public static class Queries
+    {
+
+        private  static readonly string ConnectionString;
+
+        static Queries()
+        {
+            ConnectionString =
+                "Data Source=(local);Initial Catalog=ReportDB;Integrated Security=True;MultipleActiveResultSets=True";
+        }
+        
+        public static List<ReportData> GetReportData()
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var response = db.Query<ReportData>(@"SELECT * FROM [dbo].[MainView]");
+
+                return response.ToList();
+
+            }
+            
+            
+            
+        }
+
+        public static ReportData GetReportData(int reportId)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var response = db.QueryFirst<ReportData>(
+                    @"SELECT * FROM [dbo].[MainView] Where Id = @Id",
+                    new { Id = reportId});
+
+                return response;
+
+            }
+        }
+        
+        
+        
+    }
+}
