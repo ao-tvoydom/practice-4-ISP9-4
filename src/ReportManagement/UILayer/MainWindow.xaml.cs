74798698ec545a6da2deeabe4df0d79a212ceb6b
Andrei <andreistasenko39@gmail.com>
2021-12-06 14:47:46 +0300

    Design and open file

--- src/ReportManagement/UILayer/MainWindow.xaml.cs
+++ src/ReportManagement/UILayer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.IO;
 using Microsoft.Win32;
 
@@ -12,18 +13,42 @@ namespace UILayer
         public MainWindow()
         {
             InitializeComponent();
-            
-        }
-
-        private void ImportFile_OnClick(object sender, RoutedEventArgs e)
-        {
-            
-
         }
 
         private void Exit_OnClick(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
+
+        private void Create_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        
+        private void Import_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = @"c:\";
+            openFileDialog.Multiselect = true;
+            const string defExtension = "xls";
+            openFileDialog.DefaultExt = defExtension;
+            openFileDialog.Filter = "xls files (*.xls)|*.xls|xlsx files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            Nullable<bool> result = openFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                //openFileDialog.FileNames // массив с полными путями всех выбранных файлов
+                // выполняется при нажатии ОК на выборе файлов
+            }
+            else
+            {
+                MessageBox.Show("Файлы не выбраны");
+            }
+        }
+
     }
 }
