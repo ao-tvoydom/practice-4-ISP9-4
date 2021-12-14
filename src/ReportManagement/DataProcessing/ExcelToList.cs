cafd6cae74ecc5e7fd62c34e53866fa309741b91
rigs1 <risg112@mail.ru>
2021-12-14 13:47:39 +0300

    Upd

--- src/ReportManagement/DataProcessing/ExcelToList.cs
+++ src/ReportManagement/DataProcessing/ExcelToList.cs
@@ -8,40 +8,35 @@ using ClosedXML.Excel;
 using System.IO;
 using System.Net.Security;
 using System.Threading;
+using DataProcessing.Interfaces;
 using Infrastructure.Model;
 using Microsoft.Office.Interop.Excel;
+using DataProcessing.Interfaces;
 
 namespace DataProcessing
 {
-    public static class ConvertToList
+    public class ConvertToList : IExcelConverter
     {
         public static XLWorkbook Workbook;
 
-        public static void GetFile(string filePath)
+        List<ReportData> IExcelConverter.ExcelToList()
         {
-            var fileName = filePath;
-            var workbook = new XLWorkbook(fileName);
-            Workbook = workbook;
-        }
-
-        public static List<ReportData> ExcelToList()
-        {
-            List<ReportData> list = new List<ReportData>();
-            int NameBlockStatusRow = 6;
-            int NameBrandRow = 5;
-            int NameDepartmentRow = 1;
-            int RealizationRow = 10;
-            int ProductDisposalRow = 11;
-            int ProductSurplusRow = 12;
-            int LastShipmentDateRow = 13;
-            int LastSaleDateRow = 14;
-            int SellingPriceRow = 9;
-            int NameUnitRow = 8;
-            int CodeStatusProductRow = 7;
-            int NameSectionRow = 2;
-            int CodeProductRow = 3;
-            int NameProductRow = 4;
-            int ExpirationDateRow = 15;
+           List<ReportData> list = new List<ReportData>();
+            const int nameBlockStatusRow = 6;
+            const int nameBrandRow = 5;
+            const int nameDepartmentRow = 1;
+            const int realizationRow = 10;
+            const int productDisposalRow = 11;
+            const int productSurplusRow = 12;
+            const int lastShipmentDateRow = 13;
+            const int lastSaleDateRow = 14;
+            const int sellingPriceRow = 9;
+            const int nameUnitRow = 8;
+            const int codeStatusProductRow = 7;
+            const int nameSectionRow = 2;
+            const int codeProductRow = 3;
+            const int nameProductRow = 4;
+            const int expirationDateRow = 15;
             try
             {
                 foreach (var sheet in Workbook.Worksheets)
@@ -50,46 +45,68 @@ namespace DataProcessing
                     foreach (var row in sheet.Rows())
                     {
                         ReportData item = new ReportData();
-                        if (row.Cell(CodeProductRow).Value.ToString() == String.Empty ||
-                            row.Cell(LastShipmentDateRow).Value.ToString() == "0")
+                        if (row.Cell(codeProductRow).Value.ToString() == String.Empty ||
+                            row.Cell(lastShipmentDateRow).Value.ToString() == "0")
                         {
                             continue;
                         }
 
-                        item.NameBlockStatus = row.Cell(NameBlockStatusRow).Value.ToString();
-                        item.NameBrand = row.Cell(NameBrandRow).Value.ToString();
-                        item.NameDepartment = row.Cell(NameDepartmentRow).Value.ToString();
-                        item.Realization = Convert.ToDecimal(row.Cell(RealizationRow).Value);
-                        item.ProductDisposal = Convert.ToDecimal(row.Cell(ProductDisposalRow).Value);
-                        item.ProductSurplus = Convert.ToDecimal(row.Cell(ProductSurplusRow).Value);
+                        item.NameBlockStatus = row.Cell(nameBlockStatusRow).Value.ToString();
+                        item.NameBrand = row.Cell(nameBrandRow).Value.ToString();
+                        item.NameDepartment = row.Cell(nameDepartmentRow).Value.ToString();
+                        item.Realization = Convert.ToDecimal(row.Cell(realizationRow).Value);
+                        item.ProductDisposal = Convert.ToDecimal(row.Cell(productDisposalRow).Value);
+                        item.ProductSurplus = Convert.ToDecimal(row.Cell(productSurplusRow).Value);
                         item.LastShipmentDate =
-                            DateOnly.FromDateTime(Convert.ToDateTime(row.Cell(LastShipmentDateRow).Value));
-                        if (row.Cell(LastSaleDateRow).Value.ToString() == "0")
+                            DateOnly.FromDateTime(Convert.ToDateTime(row.Cell(lastShipmentDateRow).Value));
+                        if (row.Cell(lastSaleDateRow).Value.ToString() == "0")
                         {
-                            row.Cell(LastSaleDateRow).Value = null;
+                            row.Cell(lastSaleDateRow).Value = null;
                         }
                         else
                         {
                             item.LastSaleDate =
-                                DateOnly.FromDateTime(Convert.ToDateTime(row.Cell(LastSaleDateRow).Value));
+                                DateOnly.FromDateTime(Convert.ToDateTime(row.Cell(lastSaleDateRow).Value));
                         }
 
-                        item.SellingPrice = Convert.ToDecimal(row.Cell(SellingPriceRow).Value);
-                        item.NameUnit = row.Cell(NameUnitRow).Value.ToString();
-                        item.CodeStatusProduct = Convert.ToInt32(row.Cell(CodeStatusProductRow).Value);
-                        item.NameSection = row.Cell(NameSectionRow).Value.ToString();
-                        item.CodeProduct = Convert.ToInt64(row.Cell(CodeProductRow).Value);
-                        item.NameProduct = row.Cell(NameProductRow).Value.ToString();
-                        item.ExpirationDate = Convert.ToInt32(row.Cell(ExpirationDateRow).Value);
+                        item.SellingPrice = Convert.ToDecimal(row.Cell(sellingPriceRow).Value);
+                        item.NameUnit = row.Cell(nameUnitRow).Value.ToString();
+                        item.CodeStatusProduct = Convert.ToInt32(row.Cell(codeStatusProductRow).Value);
+                        item.NameSection = row.Cell(nameSectionRow).Value.ToString();
+                        item.CodeProduct = Convert.ToInt64(row.Cell(codeProductRow).Value);
+                        item.NameProduct = row.Cell(nameProductRow).Value.ToString();
+                        item.ExpirationDate = Convert.ToInt32(row.Cell(expirationDateRow).Value);
                         list.Add(item);
                     }
                 }
 
-                return list;
+                
             }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+
+            return list;
         }
-        catch (Exception ex){
-            System.Console.WriteLine(ex);
+
+        void IExcelConverter.GetFile(string filePath)
+        {
+            GetFile(filePath);
         }
+
+        public XLWorkbook ListToExcel()
+        {
+            return new XLWorkbook();
+        }
+
+        public static void GetFile(string filePath)
+        {
+            var fileName = filePath;
+            var workbook = new XLWorkbook(fileName);
+            Workbook = workbook;
+        }
+
+        
     }
 }
---
+++ src/ReportManagement/DataProcessing/Interfaces/IExcelConverter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ClosedXML.Excel;
+using Infrastructure.Model;
+
+namespace DataProcessing.Interfaces
+{
+    public interface IExcelConverter
+    {
+        public List<ReportData> ExcelToList();
+        public void GetFile(string filePath);
+        public XLWorkbook ListToExcel();
+    }
+}
