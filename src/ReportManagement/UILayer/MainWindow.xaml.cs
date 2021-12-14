135f1de0126171bfb7a824d2204974233c8aff5a
Andrei2832 <andreistasenko39@gmail.com>
2021-12-14 13:11:03 +0300

    New window (#12)
    * Добавлен Client в конец README
    
    * Вернули Database в файл README
    
    * Создали окно
    
    * Design and open file
    
    * Add connection string
    
    * Add result window
    
    * addition to the past
    
    * Grag windows
    
    * Метод ImportFileOnClick изменён
    Константа defExtension определена выше
    исправлена неправильная передача имён
    
    * Исправлено написание ( bool? )
    
    Co-authored-by: TorinLike <boss_potehin@list.ru>

--- src/ReportManagement/UILayer/MainWindow.xaml.cs
+++ src/ReportManagement/UILayer/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace UILayer
 {
@@ -20,10 +10,69 @@ namespace UILayer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string[] _pathToFile = null!; 
+        
+        const string defExtension = "xls";
+        
+        public static MainWindow Window;
         public MainWindow()
         {
             InitializeComponent();
-            
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Window = this;
+        }
+
+        private void Drag(object sender, RoutedEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                MainWindow.Window.DragMove();
+            }
+        }
+        
+        private void CreateReport_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (_pathToFile == null)
+            {
+                MessageBox.Show("Выберите файлы");
+            }
+            else
+            {
+                ResultWindow resultWindow = new ResultWindow();
+                resultWindow.Show();
+                this.Close();
+            }
+        }
+        
+        private void ImportFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = @"c:\",
+                Multiselect = true,
+                DefaultExt = defExtension,
+                Filter = "xls files (*.xls)|*.xls|xlsx files (*.xlsx)|*.xlsx",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+
+            bool? result = openFileDialog.ShowDialog();
+
+                if (result == true) // выполняется при нажатии ОК на выборе файлов
+                {
+                    //openFileDialog.FileNames // массив с полными путями всех выбранных файлов
+                }
+                else
+                {
+                    MessageBox.Show("Файлы не выбраны");
+                }
+        }
+        private void Exit_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
---
+++ src/ReportManagement/UILayer/ResultWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace UILayer
+{
+    public partial class ResultWindow : Window
+    {
+        public static ResultWindow Window;
+        public ResultWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void ResultWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Window = this;
+        }
+        
+        private void Drag(object sender, RoutedEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                ResultWindow.Window.DragMove();
+            }
+        }
+        
+        private void SendByEmail_OnClick(object sender, RoutedEventArgs routedEventArgs)
+        {
+            
+        }
+        
+        private void UploadExcelFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        
+        private void ExitBut_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        
+    }
+}
