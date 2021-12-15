95c2af1d10983fb17a44985361c282f5bd81baaf
ilya <ilya.makhnyaev@osinit.com>
2021-12-15 10:45:30 +0300

    refactoring excel module

---
+++ src/ReportManagement/Infrastructure/Excel/ExcelReportFileConverter.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using ClosedXML.Excel;
+using Infrastructure.Interfaces;
+using Infrastructure.Model;
+
+namespace Infrastructure.Excel;
+
+public class ExcelReportFileConverter : ISourceReportFileConverter
+{
+    const int BlockStatusRow = 6;
+    const int BrandRow = 5;
+    const int DepartmentRow = 1;
+    const int RealizationRow = 10;
+    const int ProductDisposalRow = 11;
+    const int ProductSurplusRow = 12;
+    const int LastShipmentDateRow = 13;
+    const int LastSaleDateRow = 14;
+    const int SellingPriceRow = 9;
+    const int UnitRow = 8;
+    const int CodeStatusProductRow = 7;
+    const int SectionRow = 2;
+    const int CodeProductRow = 3;
+    const int ProductRow = 4;
+    const int ExpirationDateRow = 15;
+    
+    public IReadOnlyCollection<ReportData> ConvertFrom(string path)
+    {
+        if (string.IsNullOrEmpty(path)) throw new ArgumentNullException(nameof(path));
+        if (!File.Exists(path)) throw new InvalidOperationException("File in provided path is not exists");
+        
+        var list = new List<ReportData>();
+        try
+        {
+            var workbook = new XLWorkbook(path);
+            
+            foreach (var sheet in workbook.Worksheets)
+            {
+                sheet.Row(1).Delete();
+                foreach (var row in sheet.Rows())
+                {
+                    var item = new ReportData();
+                    if (row.Cell(CodeProductRow).Value.ToString() == String.Empty ||
+                        row.Cell(LastShipmentDateRow).Value.ToString() == "0")
+                    {
+                        continue;
+                    }
+
+                    item.NameBlockStatus = row.Cell(BlockStatusRow).Value.ToString()!;
+                    item.NameBrand = row.Cell(BrandRow).Value!.ToString()!;
+                    item.NameDepartment = row.Cell(DepartmentRow).Value!.ToString()!;
+                    item.Realization = Convert.ToDecimal(row.Cell(RealizationRow).Value);
+                    item.ProductDisposal = Convert.ToDecimal(row.Cell(ProductDisposalRow).Value);
+                    item.ProductSurplus = Convert.ToDecimal(row.Cell(ProductSurplusRow).Value);
+                    item.LastShipmentDate =
+                        DateOnly.FromDateTime(Convert.ToDateTime(row.Cell(LastShipmentDateRow).Value));
+                    if (row.Cell(LastSaleDateRow).Value.ToString() == "0")
+                    {
+                        row.Cell(LastSaleDateRow).Value = null;
+                    }
+                    else
+                    {
+                        item.LastSaleDate =
+                            DateOnly.FromDateTime(Convert.ToDateTime(row.Cell(LastSaleDateRow).Value));
+                    }
+
+                    item.SellingPrice = Convert.ToDecimal(row.Cell(SellingPriceRow).Value);
+                    item.NameUnit = row.Cell(UnitRow).Value.ToString()!;
+                    item.CodeStatusProduct = Convert.ToInt32(row.Cell(CodeStatusProductRow).Value);
+                    item.NameSection = row.Cell(SectionRow).Value.ToString()!;
+                    item.CodeProduct = Convert.ToInt64(row.Cell(CodeProductRow).Value);
+                    item.NameProduct = row.Cell(ProductRow).Value.ToString()!;
+                    item.ExpirationDate = Convert.ToInt32(row.Cell(ExpirationDateRow).Value);
+                    list.Add(item);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            // todo: добавить систему уведомления пользователя об ошибке или просто прокидывайте сообщение выше
+            throw;
+        }
+
+        return list;
+    }
+
+    public void ConvertTo(string path, IReadOnlyCollection<ReportData> data)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ src/ReportManagement/Infrastructure/Interfaces/ISourceReportFileConverter.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Infrastructure.Model;
+
+namespace Infrastructure.Interfaces;
+
+public interface ISourceReportFileConverter
+{
+    IReadOnlyCollection<ReportData> ConvertFrom(string path);
+    void ConvertTo(string path, IReadOnlyCollection<ReportData> data);
+}
